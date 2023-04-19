eb9c501ad1c0ab071db43af90a2f400cfc359c4a
Radhika <maddikatlaradhika@gmail.com>
2023-04-19 10:37:08 +0530

    first commit

---
+++ e5.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number: "));
+if(num==0):
+    print("Number is zero")
+elif(num%2==0):
+    print("Number is even")
+elif(num%5==0):
+    print("Number is not divisible by 5")
+else:
+    print("Number is odd")
