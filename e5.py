baff47e017d8d4d5e3a3ec1a13eb84e570e50c98
Radhika <maddikatlaradhika@gmail.com>
2023-04-19 10:37:57 +0530

    update

--- e5.py
+++ e5.py
@@ -5,5 +5,7 @@ elif(num%2==0):
     print("Number is even")
 elif(num%5==0):
     print("Number is not divisible by 5")
+elif(num%6==0):
+    print("Number is not divisible by 6")
 else:
     print("Number is odd")
